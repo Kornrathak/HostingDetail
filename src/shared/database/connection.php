aeef2eb0cdac11b2902ba066170ea184e43566a2
Kornrathak Chuenmuneewong <passlord9@gmail.com>
2017-06-08 23:03:58 +0700

    index.php

--- src/shared/database/connection.php
+++ src/shared/database/connection.php
@@ -4,12 +4,12 @@
     define('DB_USER', 'root');
     define('DB_PASSWORD', '12345678');
 
-    $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD)or die("Failed to connect to MySQL: " . mysql_error());
-    $db=mysql_select_db(DB_NAME,$con) or die ("Failed to connect to MySQL: " . mysql_error());
+    $con=mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+    $db=mysql_select_db(DB_NAME, $con) or die ("Failed to connect to MySQL: " . mysql_error());
     mysql_query("SET NAME utf8;");
 
-    // function select($command) {
-    //     echo 
-    //     return 
-    // }
+    function select($command) {
+        $rows = mysql_query("SELECT * ".$command);
+        echo $rows;
+    }
 ?>

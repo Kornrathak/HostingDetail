2fc497fae1747dd9d0abca9dc0ced44802fa9161
Kornrathak Chuenmuneewong <passlord9@gmail.com>
2017-06-09 22:05:05 +0700

    edit code

---
+++ src/bar.php
@@ -0,0 +1,48 @@
+<div class="navbar-wrapper">
+    <div class="container-fluid">
+        <nav class="navbar navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">RELAY SECTION</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.php" class="">Home</a></li>
+                        <li class=" dropdown">
+                            <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ผังบริหาร <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li class=" dropdown">
+                                    <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Add New+</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        <li><a href="#">Job Description</a></li>
+                        <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ระบบฐานข้อมูลของแผนก <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">ข้อมูลอุปกรณ์ Relay</a></li>
+                                <li><a href="#">ข้อมูลอุปกรณ์ CSCS</a></li>
+                            </ul>
+                        </li>
+                        <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ข้อมูลสำคัญ <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">คู่มือผลิตภัณฑ์ Relay</a></li>
+                                <li><a href="#">คู่มือผลิตภัณฑ์ CSCS</a></li>
+                                
+                            </ul>
+                        </li>
+                        <li><a href="#">Link หน่วยงานที่เกี่ยวข้อง</a></li>
+                        <li><a href="#">ติดต่อเรา</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
--- src/index.php
+++ src/index.php
@@ -5,6 +5,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="bar.css" />
+        <link rel="stylesheet" type="text/css" href="insert.css" />
     </head>
     <body>
         <?php
@@ -45,6 +47,107 @@
             //     'sub_name' => 'what'
             // );
             // echo '<br>'.updateDb($data, 'substation');
+        
         ?>
+
+        <div class="col-md-12 col-sm-12 ">
+        <?php include 'bar.php'; ?>
+        <br><br><br>
+        <br>
+        <br>
+        <div class="col-md-12 col-sm-12 ">
+        <h2 align="center">ตารางแสดงอุปกรณ์ </h2></div>
+
+    <div class="col-md-12 col-sm-12 ">
+      <table class="table table-bordered "><!--table-striped-->
+        <tr class="info" >
+          <th scope="col"><center>Equipment</center></th>
+          <th scope="col"><center>Name</center></th>
+          <th scope="col"><center>Panel</center></th>
+          <th scope="col"><center>Serial</center></th>
+          <th scope="col"><center>Voltage</center></th>
+        </tr>
+        <tr>
+        <?php
+ 
+    //include './shared/database/connection.php';
+    $sql=mysql_query ("select * from equipment_info ");
+
+    while($row=mysql_fetch_array($sql))
+    {
+  
+    $equit=$row['equipment'];
+    $name=$row['generation'];
+    $pa=$row['installed'];
+    $sn=$row['serial_n'];
+    $vo=$row['voltage'];
+  
+    echo "<tr><td>$equit</td> <td>$name</td> <td>$pa</td> <td>$sn</td> <td>$vo</td></tr>";
+    }
+
+  ?>
+        </tr>
+    </table>
+        </div>
+    </div>
+    <div class="col-md-12 col-sm-12 ">
+<div class="center"><button data-toggle="modal" data-target="#squarespaceModal" class="btn btn-primary center-block">กรอกข้อมูล</button></div>
+
+
+<!-- line modal -->
+<div class="modal fade" id="squarespaceModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+	<div class="modal-content">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+			<h3 class="modal-title" id="lineModalLabel">My Modal</h3>
+		</div>
+		<div class="modal-body">
+			
+            <!-- content goes here -->
+			<form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputFile">File input</label>
+                <input type="file" id="exampleInputFile">
+                <p class="help-block">Example block-level help text here.</p>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Check me out
+                </label>
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+
+		</div>
+		<div class="modal-footer">
+			<div class="btn-group btn-group-justified" role="group" aria-label="group button">
+				<div class="btn-group" role="group">
+					<button type="button" class="btn btn-default" data-dismiss="modal"  role="button">Close</button>
+				</div>
+				<div class="btn-group btn-delete hidden" role="group">
+					<button type="button" id="delImage" class="btn btn-default btn-hover-red" data-dismiss="modal"  role="button">Delete</button>
+				</div>
+				<div class="btn-group" role="group">
+					<button type="button" id="saveImage" class="btn btn-default btn-hover-green" data-action="save" role="button">Save</button>
+				</div>
+			</div>
+		</div>
+	</div>
+  </div>
+</div>
+
+</div>
+        
+
+
     </body>
 </html>

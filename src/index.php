9adccbd12c279cfd5cee8c68d9335d75c5cc4045
ukkaranut <m5135420@gmail.com>
2017-06-12 23:22:14 +0700

    index fix

--- src/index.php
+++ src/index.php
@@ -5,16 +5,162 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="./shared/js/snippet.js"></script>
         <link rel="stylesheet" type="text/css" href="shared/css/insert.css" />
+        <link rel="stylesheet" type="text/css" href="shared/css/header.css" />
+        <link rel="stylesheet" type="text/css" href="shared/css/snippet.css" />
         <link rel="stylesheet" type="text/css" href="shared/css/layout.css" />
     </head>
     <body id="myPage">
         <div class="container">
             <?php 
                 include './shared/nav/nav.php';
-                
+                include './shared/header/header.php';
+                echo '<div class="header"><h1 class="text-center">
+              <b>Relay And Automation Section</b>
+            </h1></div>';
+            
+                echo "<div class='container'>
+                        <div class='row'>
+
+                        <h2></h2>
+
+    
+                <hr>
+        <section id='pinBoot'>
+
+      <article class='white-panel'><img src='./img/bg/Relay2.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+      <article class='white-panel'><img src='./img/bg/Relay3.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+      <article class='white-panel'><img src='./img/bg/Relay4.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+
+      <article class='white-panel'><img src='./img/bg/Relay5.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+      <article class='white-panel'><img src='./img/bg/Relay6.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+      <article class='white-panel'><img src='./img/bg/Relay7.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+    <article class='white-panel'><img src='./img/bg/Relay8.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+      <article class='white-panel'><img src='./img/bg/Relay9.jpg' alt=''>
+        <h4><a href='#'>Relay and Automation Section</a></h4>
+        <p>การไฟฟ้าส่วนภูมิภาค เขต 3 (ภาคตะวันออกเฉียงเหนือ)</p>
+      </article>
+
+    </section>
+
+    <hr>
+  </div>
+
+</div> ";?>
+<div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="./img/bg/Relay8.jpg" class="img-responsive img-rounded">
+          </div>
+          <div class="col-md-6">
+            <h1>แผนกรีเลย์ และ อุปกรณ์ควบคุม</h1>
+            <h3>
+              <b>Relay and Automation Section</b>
+            </h3>
+            <p></p>
+            <h2>
+              <u>มีหน้าที่</u>
+            </h2>
+            <h3>1. ควบคุมและกำหนดค่าการทำงาน รวมถึงปรับตั้งค่า Setting ของอุปกรณ์ป้องกันในระบบจำหน่าย&nbsp;
+              <br>
+              <br>2. ติดตั้ง ทดสอบ บำรุงรักษา&nbsp;ซ่อมแซมอุปกรณ์ควบคุมในสถานีไฟฟ้า (CSCS)</h3>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <h1>RELAY</h1>
+            <h3>รีเลย์</h3>
+            <p></p>
+            <h3>คือ อุปกรณ์อิเล็คทรอนิกส์ที่ทำหน้าที่เหมือนสวิตซ์ ตัด - ต่อวงจร โดยใช้แม่เหล็กไฟฟ้า</h3>
+            <p></p>
+          </div>
+          <div class="col-md-6">
+            <img src="./img/bg/Relay7.jpg" class="img-responsive img-rounded">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="./img/bg/Relay9.jpg" class="img-responsive img-rounded">
+          </div>
+          <div class="col-md-6">
+            <h1>
+              <br>
+              <br>CSCS</h1>
+            <h3>Computer-base Substation Control System</h3>
+            <p></p>
+            <h3>คือ ระบบควบคุมอุปกรณ์ภายในสถานีไฟฟ้าด้วยคอมพิวเตอร์</h3>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <hr>
+    </div>
+    <?php
                 include './shared/footer/footer.php';
             ?>
+            
         </div>
     </body>
 </html>
---
+++ src/shared/header/header.php
@@ -0,0 +1,35 @@
+<div class="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
+  <!-- Overlay -->
+  <div class="overlay"></div>
+
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#bs-carousel" data-slide-to="1"></li>
+  </ol>
+  
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item slides active">
+      <div class="slide-1"></div>
+      <div class="hero">
+        <hgroup>
+            <h2>แผนกรีเลย์และอุปกรณ์ควบคุม</h2>        
+            <h3>Relay And Automation Section</h3>
+            
+        </hgroup>
+        <!--<button class="btn btn-hero btn-lg" role="button" data-target="#bs-carousel"> ดูหน้าที่และความรับผิดชอบ</button>-->
+      </div>
+    </div>
+    <div class="item slides">
+      <div class="slide-2"></div>
+      <div class="hero">        
+        <hgroup>
+            <h2>Provincial Electricity Authority</h2>        
+            <h3>การไฟฟ้าส่วนภูมิภาค</h3>
+        </hgroup>       
+        <!--<button class="btn btn-hero btn-lg" role="button">ติดต่อเรา</button>-->
+      </div>
+    </div>
+    
+</div>
